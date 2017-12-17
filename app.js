72c2071888796c413d5d66a35d32bfb58fd6f627
Tristan Iverson <tilevi01@gmail.com>
2017-12-16 21:41:09 -0800

    port 80

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var querystring = require('querystring');
 var url = require('url');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 var stateKey = 'spotify_auth_state';
 var app = express();
